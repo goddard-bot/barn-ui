39855b26ce85bac5ac845274decce581ecdfc9cd
rowan <rdausilio@gmail.com>
2018-01-27 17:01:43 -0500

    Update of TextBasedRobo.py

--- TextBasedRobo.py
+++ TextBasedRobo.py
@@ -1,12 +1,14 @@
 from random import *
 import time
 
+
 def init_emots():
     hap = randint(1, 100)
     hun = randint(1, 100)
     dis = randint(1, 100)
     sic = 0
     return (hap, hun, dis, sic)
+
 
 def cap_emots(hap, hun, dis, sic):
     if hap > 100:
@@ -27,48 +29,53 @@ def cap_emots(hap, hun, dis, sic):
         sic = 0
     return (hap, hun, dis, sic)
 
+
 def calc_friend(hap, hun, dis, sic):
     if hun < 25:
         hunger = -(hun * 1.25)
     else:
         hunger = hun * 1.25
     friend = (hap * 4) + (hunger) - (dis * 0.5) - (sic * 0.5)
-    friendVal = int(friend)
-    return friendVal
+    friend_val = int(friend)
+    return friend_val
+
 
 def feed_robot(hap, hun):
-    foodVal = randint(1, 20)
-    hun += foodVal
-    hap += (foodVal / 2)
-    print("You fed the robot for", foodVal, "points!")
-    print("Their happiness is now ", int(hap), " !", sep= "")
+    food_val = randint(1, 20)
+    hun += food_val
+    hap += (food_val / 2)
+    print("You fed the robot for", food_val, "points!")
+    print("Their happiness is now ", int(hap), " !", sep="")
     print("")
     return (hap, hun)
 
+
 def heal_robot(hap, dis, sic):
-    medVal = randint(1, 30)
-    sic -= medVal
-    hap += (medVal / 4)
-    dis -= (medVal / 4)
-    print("You healed the robot for", medVal, "points!")
+    med_val = randint(1, 30)
+    sic -= med_val
+    hap += (med_val / 4)
+    dis -= (med_val / 4)
+    print("You healed the robot for", med_val, "points!")
     print("Their sickness level is", sic, " now.")
     print("")
     return (hap, dis, sic)
 
+
 def move():
     pass
+
 
 def rest_robot(fri, hun, dis, sic):
     print("The robot is sleeping...")
     if fri > 100:
-        hapVal = "very happy!"
+        hap_val = "very happy!"
     elif fri > 75:
-        hapVal = "happy."
+        hap_val = "happy."
     elif fri > 50:
-        hapVal = "ok."
+        hap_val = "ok."
     else:
-        hapVal = "kinda sad."
-    print("They are " + hapVal)
+        hap_val = "kinda sad."
+    print("They are " + hap_val)
     if hun < 25:
         print("They are hungry...")
     else:
@@ -77,17 +84,18 @@ def rest_robot(fri, hun, dis, sic):
         print("And, they aren't feeling very well...")
     else:
         print("And, they're healthy!")
-    
+
 
 def play_robot():
-    text_choose = randint(1,3)
+    text_choose = randint(1, 3)
     if text_choose == 1:
         print("The robot spun around!")
     elif text_choose == 2:
         print("The robot flashed their lights happily!")
     else:
-        print("The robot did a thing.")#Later me please fix this
+        print("The robot did a thing.")  # Later me please fix this
     return (text_choose * 5)
+
 
 def main():
     happiness, hunger, discipline, sickness = init_emots()
@@ -107,8 +115,8 @@ def main():
         elif "play" in user_in:
             mod = play_robot()
             happiness += mod
-            if mod%2 == 0:
-                discipline += (mod/2)
+            if mod % 2 == 0:
+                discipline += (mod / 2)
         else:
             print("Invalid acttion. Valid actions include: ")
             print("Feed, give medicine, move, play with, and rest.")
@@ -119,17 +127,17 @@ def main():
                 user_in = user_in_2
         hunger -= 10
         stop = time.time()
-        dif = (stop - start)/2
+        dif = (stop - start) / 2
         happiness -= int(dif)
-        sickChance = randint(1, 100)
-        if sickChance > 95:
+        sick_chance = randint(1, 100)
+        if sick_chance > 95:
             sickness += 5
         if sickness != 0:
             sickness += 10
-        happiness, hunger, discipline, ickness = cap_emots(happiness,
-                                                             hunger,
-                                                             discipline,
-                                                             sickness)
+        happiness, hunger, discipline, sickness = cap_emots(happiness,
+                                                            hunger,
+                                                            discipline,
+                                                            sickness)
         friendship = calc_friend(happiness,
                                  hunger,
                                  discipline,
