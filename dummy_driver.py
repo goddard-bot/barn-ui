250fc27ccf2ef14f766e6b27afba2bc76c936914
William Stevens <contact@wastevensv.com>
2018-01-29 04:45:59 +0000

    Added a dummy driver for bot-less testing.

---
+++ dummy_driver.py
@@ -0,0 +1,38 @@
+class Robot_Driver():
+
+    def __init__(self, port='/dev/ttyUSB0'):
+        pass
+
+    def send(self, data):
+        pass
+
+    def go_forward(self, color, count):
+        pass
+
+    def reverse(self, color, count):
+        pass
+
+    def clockwise(self, color):
+        pass
+
+    def counter_clockwise(self, color):
+        pass
+
+    def nope(self, color):
+        pass
+
+    def yep(self, color):
+        pass
+
+    def get_mad(self, color):
+        pass
+
+    def update_color(self, color):
+        pass
+
+    def spin(self, color):
+        pass
+
+    def sleep(self, color):
+        pass
+
